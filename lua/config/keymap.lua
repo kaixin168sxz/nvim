05df9f5d8e86a64b34deab849f1bff3d51f6bd8b
kaixin <kaixin168KX@163.com>
2024-12-21 12:05:11 +0800

    更改文件结构，添加nvim-cmp

--- lua/config/keymap.lua
+++ lua/config/keymap.lua
@@ -15,6 +15,8 @@ vim.keymap.set('n', '<C-j>', '<C-w>j', opts)
 vim.keymap.set('n', '<C-k>', '<C-w>k', opts)
 vim.keymap.set('n', '<C-l>', '<C-w>l', opts)
 
+vim.keymap.set('i', 'jk', '<Esc>', opts)
+
 -- Resize with arrows
 -- delta: 2 lines
 vim.keymap.set('n', '<C-Up>', ':resize -2<CR>', opts)
@@ -26,6 +28,7 @@ vim.keymap.set('n', '<C-Right>', ':vertical resize +2<CR>', opts)
 -- Visual mode --
 -----------------
 
+-- 添加缩进，添加后光标位置不会回到起始位置，如`v8j>`后，光标会停留在8行下
 -- Hint: start visual mode with the same area as the previous area and the same mode
 vim.keymap.set('v', '<', '<gv', opts)
 vim.keymap.set('v', '>', '>gv', opts)
---
+++ lua/config/nvim-cmp.lua
@@ -0,0 +1,109 @@
+local has_words_before = function()
+    unpack = unpack or table.unpack
+    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+    return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
+local luasnip = require("luasnip")
+local cmp = require("cmp")
+local kind_icons = {
+  Text = "",
+  Method = "󰆧",
+  Function = "󰊕",
+  Constructor = "",
+  Field = "󰇽",
+  Variable = "󰂡",
+  Class = "󰠱",
+  Interface = "",
+  Module = "",
+  Property = "󰜢",
+  Unit = "",
+  Value = "󰎠",
+  Enum = "",
+  Keyword = "󰌋",
+  Snippet = "",
+  Color = "󰏘",
+  File = "󰈙",
+  Reference = "",
+  Folder = "󰉋",
+  EnumMember = "",
+  Constant = "󰏿",
+  Struct = "",
+  Event = "",
+  Operator = "󰆕",
+  TypeParameter = "󰅲",
+}
+
+cmp.setup({
+    snippet = {
+        -- REQUIRED - you must specify a snippet engine
+        expand = function(args)
+            require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+        end,
+    },
+    mapping = cmp.mapping.preset.insert({
+        ['<C-j>'] = cmp.mapping.scroll_docs( -4),
+        ['<C-k>'] = cmp.mapping.scroll_docs(4),
+        -- Use <CR>(Enter) to confirm selection
+        -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+        ['<CR>'] = cmp.mapping.confirm({ select = true }),
+
+        -- A super tab
+        -- sourc: https://github.com/hrsh7th/nvim-cmp/wiki/Example-mappings#luasnip
+        ["<Tab>"] = cmp.mapping(function(fallback)
+            -- Hint: if the completion menu is visible select next one
+            if cmp.visible() then
+                cmp.select_next_item()
+            elseif has_words_before() then
+                cmp.complete()
+            else
+                fallback()
+            end
+        end, { "i", "s" }), -- i - insert mode; s - select mode
+    }),
+
+  -- Let's configure the item's appearance
+  -- source: https://github.com/hrsh7th/nvim-cmp/wiki/Menu-Appearance
+  -- formatting = {
+  --   fields = { "kind", "abbr" },
+  --   format = function(entry, vim_item)
+  --     -- Kind icons
+  --     vim_item.kind = string.format('%s %s |', kind_icons[vim_item.kind], vim_item.kind) -- This concatenates the icons with the name of the item kind
+  --     -- Source
+  --     vim_item.menu = ({
+  --       buffer = "[Buffer]",
+  --       nvim_lsp = "[LSP]",
+  --       luasnip = "[LuaSnip]",
+  --       nvim_lua = "[Lua]",
+  --       latex_symbols = "[LaTeX]",
+  --     })[entry.source.name]
+  --     return vim_item
+  --   end
+  -- },
+
+    window = {
+        completion = {
+          -- winhighlight = "Normal:Pmenu,FloatBorder:Pmenu,Search:None",
+          side_padding = 0,
+        },
+      },
+      formatting = {
+        fields = { "kind", "abbr", "menu" },
+        format = function(entry, vim_item)
+          local kind = require("lspkind").cmp_format({ mode = "symbol_text", maxwidth = 50 })(entry, vim_item)
+          local strings = vim.split(kind.kind, "%s", { trimempty = true })
+          kind.kind = " " .. (strings[1] or "") .. " "
+          kind.menu = "    (" .. (strings[2] or "") .. ")"
+
+          return kind
+        end,
+      },
+
+  -- Set source precedence
+  sources = cmp.config.sources({
+    { name = 'nvim_lsp' },    -- For nvim-lsp
+    { name = 'luasnip' },     -- For luasnip user
+    { name = 'buffer' },      -- For buffer word completion
+    { name = 'path' },        -- For path completion
+  })
+})
