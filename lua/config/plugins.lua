7fcc18441d844165a0548190792ca0f032398ca2
kaixin <kaixin168KX@163.com>
2024-12-21 22:31:07 +0800

    添加grug-far插件，查找、替换文本

--- lua/config/plugins.lua
+++ lua/config/plugins.lua
@@ -207,6 +207,28 @@ require("lazy").setup({
         { "<leader>st", "<cmd>TodoTelescope<cr>", desc = "Todo" },
         { "<leader>sT", "<cmd>TodoTelescope keywords=TODO,FIX,FIXME<cr>", desc = "Todo/Fix/Fixme" },
       },
+    },
+    {
+      "MagicDuck/grug-far.nvim",
+      opts = { headerMaxWidth = 80 },
+      cmd = "GrugFar",
+      keys = {
+        {
+          "<leader>sr",
+          function()
+            local grug = require("grug-far")
+            local ext = vim.bo.buftype == "" and vim.fn.expand("%:e")
+            grug.open({
+              transient = true,
+              prefills = {
+                filesFilter = ext and ext ~= "" and "*." .. ext or nil,
+              },
+            })
+          end,
+          mode = { "n", "v" },
+          desc = "Search and Replace",
+        },
+      },
     }
 })
 
