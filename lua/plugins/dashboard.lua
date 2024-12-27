49b997ddf33ee185678a269da24ed09e3014d4fb
kaixin <kaixin168KX@163.com>
2024-12-27 20:36:25 +0800

    在dashboard按下n后，不自动打开INSER模式

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -32,7 +32,7 @@ sleeping...  z
         -- stylua: ignore
         center = {
           { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
-          { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
+          { icon = " ", key = "n", desc = "New File", action = ":ene" },
           { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
           { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
           { icon = " ", key = "c", desc = "Config", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
